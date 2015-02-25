d8193c32134f5b6f25e34c235ba121652a4bdb66
Douglas Alves <alves.douglaz@hotmail.com>
2015-02-25 15:25:08 -0300

    Projeto 02 PHP Foubdation

--- index.php
+++ index.php
@@ -13,11 +13,11 @@
 			<div class="row">
 				<nav class="collapse navbar-collapse bs-navbar-collapse">
 					<ul class="nav navbar-nav">
-						<li><a href="?nav=">Home</a></li>
-						<li><a href="?nav=empresa">Empresa</a></li>
-						<li><a href="?nav=produto">Produtos</a></li>
-						<li><a href="?nav=servicos">Serviços</a></li>
-						<li><a href="?nav=contato">Contato</a></li>
+						<li><a href="">Home</a></li>
+						<li><a href="empresa">Empresa</a></li>
+						<li><a href="produto">Produtos</a></li>
+						<li><a href="servicos">Serviços</a></li>
+						<li><a href="contato">Contato</a></li>
 					</ul>
 				</nav>
   			</div>
@@ -26,16 +26,18 @@
 				$getexe = filter_input( INPUT_GET, 'nav', FILTER_DEFAULT );
 				
 				if ( !empty( $getexe ) ):
-				    $includepatch = __DIR__ . '\\pg-' . strip_tags( trim( $getexe ) . '.php' );
+
+				    $includepatch = __DIR__ . DIRECTORY_SEPARATOR . 'pg-' . strip_tags( trim( $getexe ) . '.php' );
+
 				else:
-				    $includepatch = __DIR__ . '\\home.php';
+				    $includepatch = __DIR__ . DIRECTORY_SEPARATOR . 'home.php';
 				endif;
 				
 				if ( file_exists( $includepatch ) ):
 				    require_once($includepatch);
 				else:
 				    echo "<div>";
-				    	echo "<h1>Conteudo não encontrado</h1>";
+				    	echo "<h1>404 - PAGE NOT FOUND :(</h1>";
 				    echo "</div>";
 				endif; ?>
 				
